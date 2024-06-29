abcea7113181a2f0073808a94a868ab3c0686052
unknown <forkan.saadaoui@gmail.com>
2024-06-29 18:16:49 +0100

    Navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '@/assets/styles/globals.css'
+import Navbar from '@/components/Navbar'
 
 export const metadata = {
     title: 'Property Pulse',
@@ -14,6 +15,7 @@ const MainLayout = ({ children }) => {
   return (
     <html lang='en'>
         <body>
+            <Navbar/>
             <div>{children}</div>
         </body>
     </html>
