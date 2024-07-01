5d74f7ed4b14cebb8baad2f47a6957c4702badb6
unknown <forkan.saadaoui@gmail.com>
2024-07-01 13:00:01 +0100

    create User & Property Model

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,8 @@
 import Hero from '@/components/Hero'
 import HomeProperties from '@/components/HomeProperties'
 import InfoBoxes from '@/components/InfoBoxes'
-import connectDB from '@/config/database'
 
 const HomePage = async() => {
-  await connectDB()
   return (
     <div>
       <Hero/>
