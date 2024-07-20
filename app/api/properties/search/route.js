1bed9564c9af2397994f6a22481c654837d24105
unknown <forkan.saadaoui@gmail.com>
2024-07-20 18:48:54 +0100

    Search API Endpoint

---
+++ app/api/properties/search/route.js
@@ -0,0 +1,45 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+/**
+ * method: GET
+ * route : /api/properties/search
+ */
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(request.url);
+    const location = searchParams.get("location");
+    const propertyType = searchParams.get("propertyType");
+
+    const locationPattern = new RegExp(location, "i");
+
+    // Match location pattern against database fields
+    let query = {
+      $or: [
+        { name: locationPattern },
+        { description: locationPattern },
+        { "location.street": locationPattern },
+        { "location.city": locationPattern },
+        { "location.state": locationPattern },
+        { "location.zipcode": locationPattern },
+      ],
+    };
+
+    // Only check for property if its not 'All'
+    if (propertyType && propertyType !== 'All') {
+        const typePattern = new RegExp(propertyType, "i");
+        query.type = typePattern
+    }
+
+    const properties = await Property.find(query)
+
+    return new Response(JSON.stringify(properties), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong => Search Properties", {
+      status: 500,
+    });
+  }
+};
