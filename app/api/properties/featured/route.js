4268d1e3314af716356b1b7dd8470cfe38879b23
unknown <forkan.saadaoui@gmail.com>
2024-07-22 11:49:49 +0100

    Featured Properties

---
+++ app/api/properties/featured/route.js
@@ -0,0 +1,20 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+/**
+ * method: GET
+ * route  : /api/properties/featured
+ */
+export const GET = async (request) => {
+    try {
+      await connectDB();
+      const properties = await Property.find({
+        is_featured: true
+      })
+  
+      return new Response(JSON.stringify(properties), { status: 200 });
+    } catch (error) {
+      console.log(error);
+      return new Response("Something went wrong ==> Get featured properties", { status: 500 });
+    }
+  };
